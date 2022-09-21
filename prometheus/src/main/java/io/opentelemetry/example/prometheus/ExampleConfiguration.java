90c8d95801d78148ca5d8825d98001ed2fbecb21
minquan.chen <minquan.chen@daocloud.io>
2022-09-21 17:50:15 +0800

    exemplars

--- prometheus/src/main/java/io/opentelemetry/example/prometheus/ExampleConfiguration.java
+++ prometheus/src/main/java/io/opentelemetry/example/prometheus/ExampleConfiguration.java
@@ -5,10 +5,20 @@
 
 package io.opentelemetry.example.prometheus;
 
+import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.metrics.MeterProvider;
+import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
+import io.opentelemetry.context.propagation.ContextPropagators;
 import io.opentelemetry.exporter.prometheus.PrometheusHttpServer;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
 import io.opentelemetry.sdk.metrics.SdkMeterProvider;
+import io.opentelemetry.sdk.metrics.SdkMeterProviderBuilder;
 import io.opentelemetry.sdk.metrics.export.MetricReader;
+import io.opentelemetry.sdk.metrics.internal.SdkMeterProviderUtil;
+import io.opentelemetry.sdk.metrics.internal.exemplar.ExemplarFilter;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor;
+import io.opentelemetry.exporter.logging.LoggingSpanExporter;
 
 public final class ExampleConfiguration {
 
@@ -21,6 +31,24 @@ public final class ExampleConfiguration {
   static MeterProvider initializeOpenTelemetry(int prometheusPort) {
     MetricReader prometheusReader = PrometheusHttpServer.builder().setPort(prometheusPort).build();
 
-    return SdkMeterProvider.builder().registerMetricReader(prometheusReader).build();
+    SdkMeterProviderBuilder builder = SdkMeterProvider.builder();
+    SdkMeterProviderUtil.setExemplarFilter(builder, ExemplarFilter.sampleWithTraces());
+
+    return builder.registerMetricReader(prometheusReader).build();
+  }
+  static OpenTelemetry initOpenTelemetry() {
+    SdkTracerProvider sdkTracerProvider =
+            SdkTracerProvider.builder()
+                    .addSpanProcessor(SimpleSpanProcessor.create(new LoggingSpanExporter()))
+                    .build();
+
+    OpenTelemetrySdk sdk =
+            OpenTelemetrySdk.builder()
+                    .setTracerProvider(sdkTracerProvider)
+                    .setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance()))
+                    .build();
+
+    Runtime.getRuntime().addShutdownHook(new Thread(sdkTracerProvider::close));
+    return sdk;
   }
 }
--- prometheus/src/main/java/io/opentelemetry/example/prometheus/PrometheusExample.java
+++ prometheus/src/main/java/io/opentelemetry/example/prometheus/PrometheusExample.java
@@ -1,9 +1,24 @@
 package io.opentelemetry.example.prometheus;
 
+import io.opentelemetry.api.common.AttributeKey;
 import io.opentelemetry.api.common.Attributes;
-import io.opentelemetry.api.metrics.Meter;
-import io.opentelemetry.api.metrics.MeterProvider;
+import io.opentelemetry.api.common.AttributesBuilder;
+import io.opentelemetry.api.metrics.*;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanKind;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.context.Context;
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.sdk.metrics.SdkMeterProvider;
+import io.opentelemetry.sdk.metrics.SdkMeterProviderBuilder;
+import io.opentelemetry.sdk.metrics.internal.SdkMeterProviderUtil;
+import io.opentelemetry.sdk.metrics.internal.exemplar.ExemplarFilter;
+
+import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.function.BiConsumer;
+
+import static io.opentelemetry.api.common.AttributeKey.stringKey;
 
 /**
  * Example of using the PrometheusHttpServer to convert OTel metrics to Prometheus format and expose
@@ -15,13 +30,31 @@ import java.util.concurrent.ThreadLocalRandom;
 public final class PrometheusExample {
   private long incomingMessageCount;
 
+  private LongHistogram latencyHistogram;
+
+  private LongCounter  longCounter;
+
+  private static final OpenTelemetry openTelemetry = ExampleConfiguration.initOpenTelemetry();
+  private static final Tracer tracer =
+          openTelemetry.getTracer("io.opentelemetry.example.http.HttpServer");
+
   public PrometheusExample(MeterProvider meterProvider) {
     Meter meter = meterProvider.get("PrometheusExample");
-    meter
-        .gaugeBuilder("incoming.messages")
-        .setDescription("No of incoming messages awaiting processing")
-        .setUnit("message")
-        .buildWithCallback(result -> result.record(incomingMessageCount, Attributes.empty()));
+//    meter
+//        .gaugeBuilder("incoming.messages")
+//        .setDescription("No of incoming messages awaiting processing")
+//        .setUnit("message")
+//        .buildWithCallback(result -> result.record(incomingMessageCount, Attributes.empty()));
+
+    this.latencyHistogram =  meter.histogramBuilder("request.latency")
+            .setDescription("request latency")
+            .setUnit("ms")
+            .ofLongs()
+            .build();
+
+    this.longCounter = meter.counterBuilder("request.count")
+            .setDescription("request count")
+            .build();
   }
 
   void simulate() {
@@ -31,6 +64,40 @@ public final class PrometheusExample {
             i + " Iterations to go, current incomingMessageCount is:  " + incomingMessageCount);
         incomingMessageCount = ThreadLocalRandom.current().nextLong(100);
         Thread.sleep(1000);
+      } catch (InterruptedException e) {
+        // ignored here
+      }
+    }
+  }
+
+
+  void simulate2() {
+    for (int i = 500; i > 0; i--) {
+      try {
+        Context context = Context.current();
+        Span span =
+                tracer.spanBuilder("GET /").setSpanKind(SpanKind.SERVER).startSpan();
+        context = span.storeInContext(context);
+        long la = ThreadLocalRandom.current().nextLong(10000);
+        Attributes eventAttributes = Attributes.of(stringKey("feature"), "Exemplar");
+        if(la>8000){
+          System.out.println(
+                  i + " Iterations to go, current latency is:  " + la+" with trace id "+span.getSpanContext().getTraceId()+" span id "+span.getSpanContext().getSpanId());
+          latencyHistogram.record(la,eventAttributes,context);
+        }else {
+//          System.out.println(
+//                  i + " Iterations to go, current latency is:  " + la);
+          latencyHistogram.record(la,eventAttributes);
+
+        }
+//        if(i<5){
+//          longCounter.add(1,eventAttributes);
+//        }else {
+//          System.out.println(i + " Iterations to go with trace id "+span.getSpanContext().getTraceId()+" span id "+span.getSpanContext().getSpanId());
+//          longCounter.add(1,eventAttributes, context);
+//        }
+        Thread.sleep(1000);
+        span.end();
       } catch (InterruptedException e) {
         // ignored here
       }
@@ -51,7 +118,8 @@ public final class PrometheusExample {
 
     PrometheusExample prometheusExample = new PrometheusExample(meterProvider);
 
-    prometheusExample.simulate();
+//    prometheusExample.simulate();
+    prometheusExample.simulate2();
 
     System.out.println("Exiting");
   }
